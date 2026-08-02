dfff923a3e9b52d2bdaebf79a7f065098e98e545
Omar Ibrahim Agiza <129808006+agiza65@users.noreply.github.com>
2026-08-03 07:03:43 +0800

    Update GOOGLE_CLIENT_ID in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  GOOGLE_CLIENT_ID: "113082224752-vh23g7i1lsutgtpqc6jim5432pi5tbg4.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "903506094321-8rpfpnu42j05urln3ua4ndlpqcbp9l7b.apps.googleusercontent.com",
   GOOGLE_API_KEY: "AIzaSyBvG1SPsj8FItSpjt4fXZGKvZIE3y7OLCU",
   SPREADSHEET_ID: "1nQjhxBJyyJ5if8NK7lpF-bEVmOYeIDA8SbNr-V3t2PA",
   SHEET_RANGE: "'Data Template'!A2:F201",
